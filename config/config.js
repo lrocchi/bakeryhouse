2e26457e58d8e919568ded1aefb7e28929c2a7b1
lrocchi <lrocchi73@gmail.com>
2020-10-12 11:39:33 +0200

    Correzione Rafa

--- config/config.js
+++ config/config.js
@@ -3,8 +3,6 @@ module.exports = {
     secret: 'MarlonBrando'
   },
   database: {
-   mLab: 'mongodb://bkuser:Zaq12wsx$1@ds129593.mlab.com:29593/bkhouse', // if you want to use mLab for example
-   mLabDev: 'mongodb://bkuser:Zaq12wsx$1@ds223268.mlab.com:23268/bkhousedev', // if you want to use mLab for example
    atlasDEV: 'mongodb+srv://bkuser:Zaq12wsx$1@bkhousedev.ujvdl.mongodb.net/bkhousedev?retryWrites=true&w=majority',
    atlas: 'mongodb+srv://bkuser:Zaq12wsx$1@bkhouse.jbgau.mongodb.net/bkhouse?retryWrites=true&w=majority'
   }
